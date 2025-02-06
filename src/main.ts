4d5ba2b1ac69050a9c704334b927ad9b19d888e1
jussiisokangas <ext-jussi.isokangas@elisa.fi>
2025-02-06 08:32:55 +0200

    fix:update cors rule known url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { bootstrap } from './scripts/db-init';
 
 async function bootstrap1() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true }); // Set the correct origin for the production
+  app.enableCors({
+    origin: 'https://tkiry-frontend.onrender.com',
+    credentials: true,
+  }); // Set the correct origin for the production
   await app.listen(process.env.PORT ?? 3000);
   // bootstrap();
 }
